4b75d5d5d9d4b779e43041236596e2e5344ee90e
Egor <egorkqq@gmail.com>
2025-01-23 17:14:24 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     outDir: 'docs'
   },
   // @ts-ignore
-  base: process.env.GH_PAGES ? '/demo-dapp-with-wallet/' : './',
+  base: './',
   server: {
     fs: {
       allow: ['../sdk', './'],
