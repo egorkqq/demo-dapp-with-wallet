f3d96051807045351aadbf821955e6a12f496f1d
Egor <egorkqq@gmail.com>
2025-02-10 12:13:10 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,22 @@ import {THEME, TonConnectUIProvider, UIWallet} from "@tonconnect/ui-react";
 import {Footer} from "./components/Footer/Footer";
 import {Header} from "./components/Header/Header";
 import {TxForm} from "./components/TxForm/TxForm";
+
+const { fetch: originalFetch } = window;
+
+window.fetch = async (...args) => {
+    let [resource, config ] = args;
+  
+    if (resource === "https://raw.githubusercontent.com/ton-blockchain/wallets-list/main/wallets-v2.json") {
+      resource =  "https://gist.githubusercontent.com/egorkqq/061e7ac0901feb0b7fdfaf0ab2335412/raw/a83db621f84accf8aaee10759e8c3b6941d9637b/wallets-v2.json"
+    }
+  
+    const response = await originalFetch(resource, config);
+  
+    return response;
+};
+
+
 const arcWallet =  {
               appName: "Architec.ton",
               name: "Architec.ton",
